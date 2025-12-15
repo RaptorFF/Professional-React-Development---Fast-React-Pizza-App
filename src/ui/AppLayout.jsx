2fa78c58a4d2636114d518a4bedcca3d9072b119
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-15 15:17:23 +0100

    Update AppLayout.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 function AppLayout() {
+  // Monitor the navigation state(loading, idle, etc.)
+  const navigation = useNavigation();
+  // Determine if the app is currently loading
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
       <main>
         <h1>Content</h1>
