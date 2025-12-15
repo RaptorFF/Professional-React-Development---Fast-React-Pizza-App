ef8613c9f9472c0aff1b9df3747f1fa3c1e30bd5
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-15 15:16:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as manuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
+    // Root route with AppLayout as the main layout
     element: <AppLayout />,
+    // Error boundary for all child routes
+    errorElement: <Error />,
+    // Child routes
     children: [
       {
         path: "/",
@@ -18,15 +25,22 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        //Loader function to fetch menu data before rendering
         loader: manuLoader,
+        // Error boundary for this specific route
+        errorElement: <Error />,
       },
       {
         path: "/order/new",
         element: <CreateOrder />,
+        // Action function to handle form submission
+        action: createOrderAction,
       },
       {
         path: "/order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
