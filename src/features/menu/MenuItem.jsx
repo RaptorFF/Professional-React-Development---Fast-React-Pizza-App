6ce595e46b26d34f988c23b259db2efe8f233777
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-20 16:31:44 +0100

    Enhance MenuItem layout and add Button component
    Refactor MenuItem component for improved layout and styling.

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,16 +1,33 @@
+import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(", ")}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li key={id} className={`flex gap-4 py-2`}>
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-50 grayscale" : ""}`}
+      />
+      <div className="flex grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between text-sm">
+          {!soldOut ? (
+            <p>{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+
+          <Button disabled={soldOut} type="small" className="mt-2">
+            Add to cart
+          </Button>
         </div>
       </div>
     </li>
