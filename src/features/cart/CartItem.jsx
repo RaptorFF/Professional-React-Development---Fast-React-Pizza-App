c6154c974fbdb707d603d5f30408a968c9aa69ec
Filip Filipovic <102156199+RaptorFF@users.noreply.github.com>
2025-12-25 14:22:42 +0100

    Refactor CartItem to use UpdateItemQuantity and DeleteItem
    Replaced delete button with update quantity and delete item components.

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,15 +1,9 @@
-import { useDispatch } from "react-redux";
-import Button from "../../ui/Button";
+import DeleteItem from "./DeleteItem";
 import { formatCurrency } from "../../utils/helpers";
-import { removeItem } from "./cartSlice";
+import UpdateItemQuantity from "./UpdateItemQuantity";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-  const dispatch = useDispatch();
-
-  function handleDeleteItem() {
-    dispatch(removeItem({ pizzaId }));
-  }
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
@@ -19,9 +13,8 @@ function CartItem({ item }) {
       <div className="flex items-center justify-between text-sm sm:space-x-4">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
 
-        <Button type="small" onClick={handleDeleteItem}>
-          Delete
-        </Button>
+        <UpdateItemQuantity pizzaId={pizzaId} />
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
